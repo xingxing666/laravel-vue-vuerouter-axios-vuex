1546a304e270b37006deb225159d3b85a916e6b4
cuishuai <415145850@qq.com>
2018-11-30 03:27:10 +0000

    first push

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,20 @@
+<style scoped>
+
+</style>
+<template>
+	<div id="app">
+		<router-view/>
+	</div>
+</template>
+<script>
+export default {
+	data () {
+		return {
+		    
+		};
+	},
+	computed: {
+	  
+	}
+}
+</script>
